1c3ba8ecf902da31265cce35b96a8954deb865f3
Ryan Schmukler <rschmukler@gmail.com>
2014-05-18 20:47:15 -0400

    fix exporting of utils onto builder

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('./lib/builder.js');
+var Builder = module.exports = require('./lib/builder.js');
+
+Builder.utils = {
+  clone: require('./utils/clone.js'),
+  isGenerator: require('./utils/isGenerator.js'),
+  type: require('./utils/type.js')
+};
