6a48a7f6a34a3217ad9d613c6366b233436d02d2
Ryan Schmukler <rschmukler@gmail.com>
2014-05-18 20:51:26 -0400

    fix util requires

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Builder = module.exports = require('./lib/builder.js');
 
 Builder.utils = {
-  clone: require('./utils/clone.js'),
-  isGenerator: require('./utils/isGenerator.js'),
-  type: require('./utils/type.js')
+  clone: require('./lib/utils/clone.js'),
+  isGenerator: require('./lib/utils/isGenerator.js'),
+  type: require('./lib/utils/type.js')
 };
